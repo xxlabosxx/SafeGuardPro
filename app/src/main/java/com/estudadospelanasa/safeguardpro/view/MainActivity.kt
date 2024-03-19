ee3e20ba4af71ceb3c801bdc390c58c9987bb498
Gustavo Labos <gustavo.labos@portalsesisp.org.br>
2024-03-19 14:31:51 -0300

    Configurar o projeto para MVVM
    Configurar a navegação.

--- app/src/main/java/com/estudadospelanasa/safeguardpro/view/MainActivity.kt
+++ app/src/main/java/com/estudadospelanasa/safeguardpro/view/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.estudadospelanasa.safeguardpro
+     package com.estudadospelanasa.safeguardpro.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.estudadospelanasa.safeguardpro.R
 
-class MainActivity : AppCompatActivity() {
+     class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
