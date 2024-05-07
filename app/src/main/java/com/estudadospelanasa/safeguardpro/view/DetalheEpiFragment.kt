8c3bae6993436c27a527af1c348e420122acf65b
Gustavo Labos <gustavo.labos@portalsesisp.org.br>
2024-05-07 14:35:53 -0300

    tirando missing contraint e position absolute

---
+++ app/src/main/java/com/estudadospelanasa/safeguardpro/view/DetalheEpiFragment.kt
@@ -0,0 +1,13 @@
+//package com.estudadospelanasa.safeguardpro.view
+//
+//import android.os.Bundle
+//import androidx.fragment.app.Fragment
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import com.estudadospelanasa.safeguardpro.R
+//
+//class DetalheEpiFragment : Fragment() {
+//
+//    }
+//}
